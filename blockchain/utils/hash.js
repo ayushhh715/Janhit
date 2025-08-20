979fe06de96fc49097eec27a908d12823eb4fde6
ayushhh715 <22211@iiitu.ac.in>
2025-08-20 19:31:14 +0530

    hjgyj

--- blockchain/utils/hash.js
+++
@@ -1,7 +0,0 @@
-const crypto = require("crypto");
-
-function hashIssue(issueData) {
-  return crypto.createHash("sha256").update(JSON.stringify(issueData)).digest("hex");
-}
-
-module.exports = { hashIssue };
